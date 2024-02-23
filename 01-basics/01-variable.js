1d717e872f81517216d435501be658bace5f4d40
Arindam47 <150152451+Arindam47@users.noreply.github.com>
2024-02-23 13:44:39 +0000

    container config done

---
+++ 01-basics/01-variable.js
@@ -0,0 +1,8 @@
+const accoountId = 353265820936;
+let accountEmail = "Arindam@google.com";
+let accountPassword = "Arindam555";
+accountCity = "Guwahati";
+
+let accountState;
+
+console.table(["accountId","accountEmail","accountPassword"]);
